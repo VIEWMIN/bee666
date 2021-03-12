17c89a8a5854bc0a0d00beb668eb5c0fb099422d
VIEWMIN <justonekiss66@gmail.com>
2021-03-12 23:24:57 +0700

    bee666

--- src/main.js
+++ src/main.js
@@ -2,8 +2,28 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from 'bootstrap-vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
 
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA1RmAM6Rq77qYI77psmYvp5-jVmMbJ-IU",
+  authDomain: "bee666-b0800.firebaseapp.com",
+  projectId: "bee666-b0800",
+  storageBucket: "bee666-b0800.appspot.com",
+  messagingSenderId: "732392778959",
+  appId: "1:732392778959:web:d60a51588335a30dd15bba",
+  measurementId: "G-459RYBFMZ8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const db = firebase.firestore()
 
 new Vue({
   router,
---
+++ src/views/V9.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="bg">
+    <div class="container">
+      <img src="http://www.welovesuphan.com/images/123449734.png" />
+      <b-form>
+        <b-form-group id="input-group-2" label="Car ID" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="CarID"
+            type="CarID"
+            placeholder="Enter CarID"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Band" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="Band"
+            type="text"
+            placeholder="Enter Band"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-2"
+          label="color"
+          label-for="input-2"
+        >
+          <b-form-input
+            id="input-2"
+            v-model="color"
+            type="text"
+            placeholder="Enter color"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Price" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="Price"
+            placeholder="Enter Price"
+            type="Number"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button class="btn" variant="primary" @click="onSubmit">sign up</b-button>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { db } from "@/main.js";
+export default {
+  data() {
+    return {
+      CarID: "",
+      Band: "",
+      color: "",
+      Price: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      var form = {
+        CarID: this.CarID,
+        Band: this.Band,
+        color: this.color,
+        Price: this.Price,
+      };
+      db.collection("bee666").doc().set(form);
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  background: rgb(166, 247, 247);
+}
+</style>
